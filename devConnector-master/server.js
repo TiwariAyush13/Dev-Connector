04581c56ddddc1cd0c027546e2a5163905e2939f
Ayush Tiwari <ayushonline.net@gmail.com>
2020-08-11 12:14:38 +0530

    linkedIn Social network
    linkedIn Social network

---
+++ devConnector-master/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const connectDB = require('./config/db');
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+
+//Connect to Database
+connectDB();
+
+//Initialize middlewares
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send("Server Running");
+})
+
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/auth', require('./routes/api/auth'));
+
+app.listen(PORT, () => console.log(`Server stared at port ${PORT}`));
